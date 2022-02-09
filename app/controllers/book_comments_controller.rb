3625e021cd8261d0261e7cb5a93d2d6ab04770a5
EC2 Default User <ec2-user@ip-172-31-21-167.ec2.internal>
2022-02-09 05:22:52 +0000

    [FINISH]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,23 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      flash.now[:notice] = "コメントを投稿しました"
+      render :book_comments #render先にjsファイルを指定
+    else
+      render :error   #render先にjsファイルを指定
+    end
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    flash.now[:alert] = "投稿を削除しました"
+    #renderしたときに@bookのデータがないので@bookを定義
+    @book = Book.find(params[:book_id])
+    render :book_comments  #render先にjsファイルを指定
   end
 
 
